1c308410c8bcb8aac72b2a8b2c42a6d03bb9a5a4
Mario Shang <mingjieshang@163.com>
2024-04-16 14:34:19 +0800

    fix bug

--- app/xray/config.py
+++ app/xray/config.py
@@ -226,7 +226,9 @@ class XRayConfig(dict):
                                 f"You need to provide publicKey in realitySettings of {inbound['tag']}")
 
                     try:
-                        settings['sid'] = tls_settings.get('shortIds')[0]
+                        for shortId in tls_settings.get('shortIds', []):
+                          if shortId != '':
+                            settings['sid'] = shortId
                     except (IndexError, TypeError):
                         raise ValueError(
                             f"You need to define at least one shortID in realitySettings of {inbound['tag']}")
